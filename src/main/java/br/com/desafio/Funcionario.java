952da8a27bb3c6381dea7b3767dc809609a08420
José Rafael <joseraffael2003@gmail.com>
2026-09-17 17:56:18 -0300

    Adiciona classe Funcionario com herança de Pessoa

---
+++ src/main/java/br/com/desafio/Funcionario.java
@@ -0,0 +1,31 @@
+package br.com.desafio;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
